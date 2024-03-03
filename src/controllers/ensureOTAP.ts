35afe3bc56c10386965c7ac53e42ea81e4ff3194
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-03-03 18:37:59 +0100

    d612d70e-6cc8-4ae7-af1b-a8d01d8abfb8-auto-init

--- src/controllers/ensureOTAP.ts
+++ src/controllers/ensureOTAP.ts
@@ -5,12 +5,12 @@ import { BranchSummary } from "simple-git";
 
 async function createBranch( git:Git , branch:BranchSummary , branchName:string ){
 
-  // let checkout = await git.checkoutBranch( branchName , `origin/${branch.current}` );
-  // console.log({ checkout });
-  // let add = await git.add('.');
-  // console.log({ add });
-  // let commit = await git.commit(`${randomUUID()}-auto-init`);
-  // console.log({ commit });
+  let checkout = await git.checkoutBranch( branchName , `origin/${branch.current}` );
+  console.log({ checkout });
+  let add = await git.add('.');
+  console.log({ add });
+  let commit = await git.commit(`${randomUUID()}-auto-init`);
+  console.log({ commit });
   let push = await git.push( `origin` , branchName );
   console.log({ push });
 
