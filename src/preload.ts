963fcd1b5dd91d1f7abfcca0c1124b1bad2675d6
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-03-03 18:48:41 +0100

    push

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const USERNAME = "MortallicaXxX";
+const TOKEN = "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"
+
+process.env.GIT_CREDENTIALS = `${USERNAME}:${TOKEN}`;
+
 export const cwdPath = (process.env.PWD ? process.env.PWD : process.cwd());
 
 export type VersionManagerConfig = {
